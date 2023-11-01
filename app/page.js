127f0547caff65c4546c4255a3792e7a655c34f6
rosette777 <wkwk2805u@zcube.co.kr>
2023-11-01 20:04:27 +0900

    file 확인

--- app/page.js
+++ app/page.js
@@ -1,7 +1,21 @@
+"use client";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const files = e.target[0].files;
+          console.log(files);
+        }}
+      >
+        <input type="file" name="file" />
+        <input type="submit" value="전송" />
+      </form>
+    </div>
+  );
 };
 
 export default Home;
