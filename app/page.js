8c2a66d79a295406bbeed5b9cec3cedc00f12d6d
rosette777 <wkwk2805u@zcube.co.kr>
2023-11-02 19:40:04 +0900

    디자인 변경

--- app/page.js
+++ app/page.js
@@ -1,47 +1,59 @@
 "use client";
+import React from "react";
 import { pinFileToIPFS, pinJSONToIPFS } from "@/utils/pinata";
 import { InjectedConnector } from "wagmi/connectors/injected";
-import React from "react";
-import { useAccount, useConnect } from "wagmi";
+import { useAccount, useBalance, useConnect } from "wagmi";
 
 const Home = () => {
-  const { isConnected } = useAccount();
+  const { address, isConnected } = useAccount();
+  const { data: balance } = useBalance({
+    address,
+  });
   const { connect } = useConnect({
     connector: new InjectedConnector(),
   });
   return (
-    <div>
+    <div style={{ width: "70%", margin: "auto" }}>
       {isConnected ? (
-        <form
-          onSubmit={async (e) => {
-            e.preventDefault();
-            const [name, description, file] = e.target;
-            const { IpfsHash: fileCID } = await pinFileToIPFS(file.files[0]);
-            const jsonData = {
-              name: name.value,
-              description: description.value,
-              image: `https://ipfs.io/ipfs/${fileCID}`,
-            };
-            const { IpfsHash: jsonCID } = await pinJSONToIPFS(jsonData);
-            console.log("json", jsonCID);
-          }}
-        >
-          <p>
-            <input type="text" name="name" placeholder="name" />
-          </p>
-          <p>
-            <textarea
-              name="description"
-              cols="30"
-              rows="10"
-              placeholder="description"
-            ></textarea>
-          </p>
-          <p>
-            <input type="file" name="file" />
-          </p>
-          <input type="submit" value="전송" />
-        </form>
+        <div>
+          <h4>Address</h4>
+          <div>{address}</div>
+          <h4>Balance</h4>
+          <div>
+            {balance?.formatted} {balance?.symbol}
+          </div>
+          <h4>NFT 생성</h4>
+          <form
+            onSubmit={async (e) => {
+              e.preventDefault();
+              const [name, description, file] = e.target;
+              const { IpfsHash: fileCID } = await pinFileToIPFS(file.files[0]);
+              const jsonData = {
+                name: name.value,
+                description: description.value,
+                image: `https://ipfs.io/ipfs/${fileCID}`,
+              };
+              const { IpfsHash: jsonCID } = await pinJSONToIPFS(jsonData);
+              console.log("json", jsonCID);
+            }}
+          >
+            <p>
+              <input type="text" name="name" placeholder="name" />
+            </p>
+            <p>
+              <textarea
+                name="description"
+                cols="30"
+                rows="10"
+                placeholder="description"
+              ></textarea>
+            </p>
+            <p>
+              <input type="file" name="file" />
+            </p>
+            <input type="submit" value="전송" />
+          </form>
+        </div>
       ) : (
         <button onClick={connect}>연결</button>
       )}
